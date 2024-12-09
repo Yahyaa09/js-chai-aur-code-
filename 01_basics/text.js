f9266c05675be6a7ed89a0762db233c1b287b422
Mohammad Yahyaa <165954553+Yahyaa09@users.noreply.github.com>
2024-12-09 16:48:04 +0000

    conversions

--- 01_basics/text.js
+++ 01_basics/text.js
@@ -1,1 +1,29 @@
-console.log("Yahyaa");
+let accoutnId = "12345";
+var namer = "Yahyaa";
+const number = 989739;
+
+console.table([accoutnId,namer,number]);
+
+let value = Number(accoutnId)
+console.log(value)
+
+let str1 = "Hello";
+let str2 = " Yahyaa";
+
+console.log(str1 + str2);
+
+//confusing meme solution **operations**
+console.log("1" + 2);
+
+console.log(1 + "2");
+
+console.log("1" + 2 + 2); //if the first letter is string then treat all as string
+
+console.log(1 + 2 + "2"); //if the first letter is number then calculate number first treat all as string
+
+let num = 1 + 2 + "3"
+
+console.log(typeof num);
+
+
+
