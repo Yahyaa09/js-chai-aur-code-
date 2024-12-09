09db988e45016f7fbe780dfd1794794460d2f74f
Mohammad Yahyaa <165954553+Yahyaa09@users.noreply.github.com>
2024-12-09 16:47:11 +0000

    learning typeof()

---
+++ interview_specific/datatypes.js
@@ -0,0 +1,37 @@
+//only 2 types of datatypes : primitive(pass by value) and non primitive(pass by refernece) 
+
+//primitive : string , Number , bool , Null , undefined , Symbol , BigInt
+
+//Non-primitive : Arrays , Objects , Functions 
+
+//wE'LL SEE typeof OF ALL DATATYPES
+
+let num = 123;
+
+let str = "Yahyaa";
+
+let arr = ["yahyaa","ashhar","pulkit"];
+
+let myObj = {
+    name : "Yahyaa",
+    age:22
+}
+
+const myfunc = Function()
+{
+    console.log("inside function 1")
+}
+
+const nullValue = null;
+
+//let's check
+const data = [
+    { Variable: "num", Type: typeof num }, //output : number 
+    { Variable: "str", Type: typeof str }, //output : string 
+    { Variable: "arr", Type: typeof arr }, //output : object 
+    { Variable: "myObj", Type: typeof myObj }, //output : object 
+    { Variable: "myfunc", Type: typeof myfunc }, //output : function
+    { Variable: "nullValue", Type: typeof nullValue } //output : object 
+  ];
+  
+  console.table(data);
