20c8238b3deb647e20def72da83e6387aaaa4059
Mohammad Yahyaa <165954553+Yahyaa09@users.noreply.github.com>
2024-12-11 01:43:30 +0000

    maths added

--- 01_basics/strings_and_maths.js
+++ 01_basics/strings_and_maths.js
@@ -27,3 +27,31 @@
 
  let splittedUrl = newUrl.split(' ')
  console.log(splittedUrl)
+
+ //************************************  MATHS  ***********************************
+ let score = new Number(200)
+
+ console.log(score.toString().length) // conversion
+
+ console.log(score.toFixed(2))  // to 2 decimal place
+
+ let num =10000000;
+
+ console.log(num.toLocaleString('en-IN')) //add commas in num
+
+ //ceil floor min max etc
+
+ let num2 = 23456
+
+ console.log(Math.random()) //values between 0 and 1
+
+ console.log(Math.floor(Math.random()*10) + 1) // this will be greater than 1 always
+
+
+//to generate a random integer between a and b
+let max = 20
+let min = 10
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min))
+
+
