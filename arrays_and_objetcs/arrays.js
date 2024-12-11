4cf83a79028d8d9491344f601534bedd2fc99824
Mohammad Yahyaa <165954553+Yahyaa09@users.noreply.github.com>
2024-12-11 06:27:33 +0000

    arrays done

--- arrays_and_objetcs/arrays.js
+++ arrays_and_objetcs/arrays.js
@@ -54,6 +54,51 @@ console.log(arr);
 
 //SO THE ORIGINAL ARRAY IS AFFECTED OR CHANGED PERMANENTLY IN SPLICE ASLO SPLICE INCLUDES THE LAST INDEX TOO 
 
+/********************************************************/
+
+
+//Arrays can take any type of data in JS 
+let arr1 = ["Yahyaa","Khushi","Ashhar",1,true];
+let arr2 = ["Maaz","Shubham","Pulkit"];
+
+
+let arr3 = arr2.concat(arr1); //This returns a New Array
+
+console.log(arr3);
+
+//USE SPREAD OPERATOR ( makes shallow copies )
+let arr4 = [...arr1,...arr2];
+console.log(arr4);
+
+let arr5 =[8,9,10,...arr1];
+console.log(arr5);
+
+/**************************************************** */
+
+let layredArr = [1,2,3,[4,5,[6,7,8]]];
+
+//flatten or concat all elements into one array
+
+let flattenedArr = layredArr.flat(Infinity) //give depth in arg
+
+console.log(flattenedArr)
+
+
+//Check if somethign is an array
+
+console.log(Array.isArray("yAHYAA"));
+
+console.log(Array.from("Yahyaa")); //make array
+
+console.log(Array.from({name : "Yahyaa"}));  // returns [] when it cant convert into array]
+
+let num2 = 100;
+let num3 = 200;
+let num4 = 300;
+
+
+let newArray = Array.of(num2,num3,num4); //returns new array with combined values
+console.log(newArray);
 
 
 
